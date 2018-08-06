b98b03d15c994e4c91fe93e0177c3963ce09b4d7
NGUYEN XUAN HUY p1419771 <xuan-huy.nguyen@etu.univ-lyon1.fr>
2018-08-06 19:01:53 +0200

    adding button->element interactive

--- Day_2/Chapter 3/soccerviz.js
+++ Day_2/Chapter 3/soccerviz.js
@@ -31,4 +31,26 @@ function overallTeamViz(data) {
     .style('text-anchor', 'middle')
     .style('font-size', '10px')
     .text(function(d) { return d.team })
+
+  let dataKeys = d3.keys(data[0]).filter(function(e) {
+    return e !== 'team' && e !== 'region'
+  })
+
+  d3.select('#controls')
+    .selectAll('button.teams')
+    .data(dataKeys)
+    .enter()
+    .append('button')
+    .on('click', buttonClick)
+    .html(function(d) { return d })
+
+  function buttonClick(datapoint) {
+    let maxVal = d3.max(data, function(d) { return parseFloat(d[datapoint]) })
+    let radiusScale = d3.scaleLinear().domain([0, maxVal]).range([2,20])
+
+    d3.selectAll('g.overallG')
+      .select('circle')
+      .attr('r', function(d) { return radiusScale(d[datapoint]) })
+  }
 }
+
