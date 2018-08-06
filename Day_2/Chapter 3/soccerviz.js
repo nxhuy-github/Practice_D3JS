39f8819313e9a4b7d5ffed1e5ef3b7bd36c8a35a
NGUYEN XUAN HUY p1419771 <xuan-huy.nguyen@etu.univ-lyon1.fr>
2018-08-06 18:22:27 +0200

    D3 in Action Examples

---
+++ Day_2/Chapter 3/soccerviz.js
@@ -0,0 +1,34 @@
+function createSoccerViz() {
+  d3.csv('worldcup.csv', function(data) {
+    overallTeamViz(data)
+  })
+}
+
+function overallTeamViz(data) {
+  d3.select('svg')
+    .append('g')
+    .attr('id', 'teamsG')
+    .attr('transform', "translate(50,300)")
+    .selectAll('g')
+    .data(data)
+    .enter()
+    .append(g)
+    .attr('class', 'overallG')
+    .attr('transform', function(d, i) {
+      return 'translate(' + (i*50) + ', 0)'
+    })
+
+  let teamG = d3.selectAll('g.overallG')
+
+  teamG.append('circle')
+    .attr('r', 20)
+    .style('fill', 'pink')
+    .style('stroke', 'black')
+    .style('stroke-width', '1px')
+
+  teamG.append('text')
+    .attr('y', 30)
+    .style('text-anchor', 'middle')
+    .style('font-size', '10px')
+    .text(function(d) { return d.team })
+}
